7a8d5fe658b6021980e29ed1b2a66ff48edb2b69
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2021-10-04 21:28:42 -0400

    Create index.js

---
+++ players/index.js
@@ -0,0 +1,27 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+getJSON('https://api.mcsrvstat.us/2/play.endercraftsmp.tk', function(err, data) {
+var player = 0
+  while (player <= data.players.online-1) {
+    var playername = data.players.list[player]
+    console.log(playername)
+    var playerelement = document.createElement('div')
+    playerelement.innerHTML = playername
+    document.body.appendChild(playerelement)
+  
+    player += 1
+   
+  }
+})
